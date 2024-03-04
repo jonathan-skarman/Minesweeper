0b17d026f992d24f79d6354ea243d8301a594356
jonathan skarman <kaboomgf@gmail.com>
2024-03-04 10:57:11 +0100

    pregenerate where numbers are

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,4 @@
-height = 5
-width = 5
-mines = 5
+from random import randint
 
 def create_column(length): #creates a column of "False" that is length long
 	i = 0
@@ -10,10 +8,66 @@ def create_column(length): #creates a column of "False" that is length long
 		i += 1
 	return column
 
-def create_board(height, width):
+def create_board(height, width): #creates a board with only false
 	i = 0
 	board = []
 	while i < width:
 		board.append(create_column(height))
 		i += 1
 	return board
+
+def place_mines(height, width, mines): #sets where the mines are
+	i = 0
+	board = create_board(height, width)
+	while i < mines:
+		board[randint(0, width - 1)][randint(0, height - 1)] = True
+		i += 1
+	return board
+
+def generate_nums(board):
+	width = len(board)
+	height = len(board[0])
+	x = 0
+	y = 0
+	while x < width:
+		while y < height:
+			if board[x][y] != True:
+				board[x][y] = adjacent_amount(board, x, y)
+			y += 1
+		x += 1
+
+def adjacent_amount(board, x, y):
+	width = len(board) - 1
+	height = len(board[0]) - 1
+
+	amount = 0
+	if (x != 0) and (y != 0) and (board[x-1][y-1]): #top left
+		amount += 1
+	if (x != 0) and (board[x-1][y]): #middle left
+		amount += 1
+	if (x != 0) and (y != height) and (board[x-1][y+1]):# bottom left
+		amount += 1
+	if (y != 0) and (board[x][y-1]): #top middle
+		amount += 1
+	if (y != height) and (board[x][y+1]): #bottom middle
+		amount += 1
+	if (x != width) and (y != 0) and (board[x+1][y-1]): #top right
+		amount += 1
+	if (x != width) and (board[x+1][y]): #middle right
+		amount += 1
+	if (x != width) and (y != height) and (board[x+1][y+1]):
+		amount += 1
+	return amount
+
+
+
+def runner(height, width, mines):
+	board = generate_nums(place_mines(height, width, mines))
+	opened = create_board(height, width)
+
+
+
+height = 5
+width = 5
+mines = 5
+#runner(height, width, mines)
